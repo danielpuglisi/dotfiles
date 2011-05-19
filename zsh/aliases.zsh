cbbf27b38b2ed778389413e3979a67de01f3a7c6
Rodrigo Haenggi <rodrigo@codegestalt.com>
2011-05-19 23:29:12 +0200

    created an useful alias for ctags

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,5 +8,7 @@ alias rack="ack --ruby --follow"
 alias jack="ack --js"
 alias pack="ack --python"
 
+# default ctags alias
+alias ctags="ctags -R --exclude=.git --exclude=log *"
 
 alias reload!='. ~/.zshrc'
