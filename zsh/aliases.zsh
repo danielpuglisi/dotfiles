1ffe0cab0041873a1253fa65815125f62ee18860
Rodrigo Haenggi <rodrigo@codegestalt.com>
2011-05-19 22:54:09 +0200

    ACK madness!

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,12 @@
 alias dotmate='mate $ZSH'
 alias dotcd='cd $ZSH'
 
+
+#ack madness!
+alias aack="ack --all"
+alias rack="ack --ruby --follow"
+alias jack="ack --js"
+alias pack="ack --python"
+
+
 alias reload!='. ~/.zshrc'
