0ba1225053b3c1c3d80b4df6ceb9fd430d0f0a0d
Rodrigo Haenggi <rodrigo@codegestalt.com>
2011-07-27 23:28:21 +0200

    Clean out old vim plugins

--- vim/vim.symlink/update_bundles.rb
+++ vim/vim.symlink/update_bundles.rb
@@ -17,12 +17,7 @@ git_bundles = [
     "git://github.com/tpope/vim-repeat.git",              # enable repeating supported plugin maps with "."
     "git://github.com/scrooloose/nerdcommenter.git",      # Vim plugin for commenting
     "git://github.com/vim-ruby/vim-ruby.git",             # Edit and compile Ruby within Vim.
-]
-
-vim_org_scripts = [
-  ["IndexedSearch", "7062",  "plugin"],
-  ["gist",          "12732", "plugin"],
-  ["jquery",        "12107", "syntax"],
+    "git://github.com/vim-scripts/Gist.vim.git",        # Vimscript for gist
 ]
 
 require 'fileutils'
@@ -40,13 +35,4 @@ git_bundles.each do |url|
   puts "  Unpacking #{url} into #{dir}"
   `git clone #{url} #{dir}`
   FileUtils.rm_rf(File.join(dir, ".git"))
-end
-
-vim_org_scripts.each do |name, script_id, script_type|
-  puts "  Downloading #{name}"
-  local_file = File.join(name, script_type, "#{name}.vim")
-  FileUtils.mkdir_p(File.dirname(local_file))
-  File.open(local_file, "w") do |file|
-    file << open("http://www.vim.org/scripts/download_script.php?src_id=#{script_id}").read
-  end
 end
