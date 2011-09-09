4dfc3b572c637a8b3cc5c8f656754ebf0f4122e9
Rodrigo Haenggi <rodrigo@codegestalt.com>
2011-09-09 06:46:24 +0200

    Update path variable for tomcat6 installation

--- system/path.zsh
+++ system/path.zsh
@@ -4,7 +4,7 @@ export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export PATH="/usr/local/mysql/bin:$PATH"
 
 # TOMCAT
-export PATH="/usr/local/tomcat6/bin:$PATH"
+export PATH="/Users/rodrigo/tomcat6/bin:$PATH"
 
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
