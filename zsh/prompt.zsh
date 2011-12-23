3e487707ef1989f7fa9d4493b9c23a1aa9b24f14
Rodrigo Haenggi <rodrigo@codegestalt.com>
2011-12-23 21:31:10 +0100

    pimp git display

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -14,9 +14,9 @@ git_dirty() {
   else
     if [[ $st == "nothing to commit (working directory clean)" ]]
     then
-      echo "%{$fg[green]%}$(git_prompt_info)%{$reset_color%}"
+      echo "(%{$fg[green]%}$(git_prompt_info)%{$reset_color%})"
     else
-      echo "%{$fg[red]%}$(git_prompt_info)%{$reset_color%}"
+      echo "(%{$fg[red]%}$(git_prompt_info)%{$reset_color%})"
     fi
   fi
 }
@@ -25,30 +25,6 @@ git_prompt_info () {
  ref=$(/usr/bin/git symbolic-ref HEAD 2>/dev/null) || return
   # echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
  echo "${ref#refs/heads/}"
-}
-
-function minutes_since_last_commit {
-    now=`date +%s`
-    last_commit=`git log --pretty=format:'%at' -1`
-    seconds_since_last_commit=$((now-last_commit))
-    minutes_since_last_commit=$((seconds_since_last_commit/60))
-    echo $minutes_since_last_commit
-}
-rod_git_prompt() {
-    local g="$(__gitdir)"
-    if [ -n "$g" ]; then
-        local MINUTES_SINCE_LAST_COMMIT=`minutes_since_last_commit`
-        if [ "$MINUTES_SINCE_LAST_COMMIT" -gt 30 ]; then
-            local COLOR=$fg[red]
-        elif [ "$MINUTES_SINCE_LAST_COMMIT" -gt 10 ]; then
-            local COLOR=$fg[yellow]
-        else
-            local COLOR=$fg[green]
-        fi
-        local SINCE_LAST_COMMIT="${COLOR}$(minutes_since_last_commit)m$reset_color"
-        local GIT_PROMPT="($fg[green]`git_dirty`$reset_color|${SINCE_LAST_COMMIT})"
-        echo ${GIT_PROMPT}
-    fi
 }
 
 project_name () {
@@ -83,34 +59,13 @@ rvm_prompt(){
   fi
 }
 
-# This keeps the number of todos always available the right hand side of my
-# command line. I filter it to only count those tagged as "+next", so it's more
-# of a motivation to clear out the list.
-todo(){
-  if $(which todo.sh &> /dev/null)
-  then
-    num=$(echo $(todo.sh ls +next | wc -l))
-    let todos=num-2
-    if [ $todos != 0 ]
-    then
-      echo "$todos"
-    else
-      echo ""
-    fi
-  else
-    echo ""
-  fi
-}
-
 directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-#export PROMPT=$'\n$(rvm_prompt) in $(directory_name) $(project_name_color)$(git_dirty)$(need_push) › '
-export PROMPT=$'\n$(directory_name) $(rod_git_prompt)$(need_push)\n› '
-set_prompt () {
-  export RPROMPT="%{$fg[yellow]%}$(todo)%{$reset_color%}"
-}
+ set_prompt () {
+  export PROMPT=$'\n$(directory_name) $(git_dirty)$(need_push) › '
+ }
 
 precmd() {
   title "zsh" "%m" "%55<...<%~"
