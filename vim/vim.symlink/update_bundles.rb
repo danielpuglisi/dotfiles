373a78f49d863e635978bed0269c8adc1f627960
Rodrigo Haenggi <rodrigo@codegestalt.com>
2012-02-22 14:19:00 +0100

    reorder bundles

--- vim/vim.symlink/update_bundles.rb
+++ vim/vim.symlink/update_bundles.rb
@@ -10,13 +10,13 @@ git_bundles = [
     "git://github.com/tpope/vim-fugitive.git",                    # Git wrapper
     "git://github.com/tpope/vim-rails.git",                       # Ruby on Rails power tools
     "git://github.com/vim-ruby/vim-ruby.git",                     # Edit and compile Ruby within Vim.
-
-    # "git://github.com/msanders/snipmate.vim.git",               # TextMate Snippets for Vim
     "git://github.com/vim-scripts/taglist.vim.git",               # Source code browser
     "git://github.com/mileszs/ack.vim.git",                       # Vim plugin for the Perl module / CLI script "ack"
-    # "git://github.com/tpope/vim-surround.git",                  # quoting/parenthesizing made simple
     "git://github.com/tpope/vim-markdown.git",                    # Vim Markdown runtime files
     "git://github.com/lepture/vim-velocity.git",                  # Velocity stuff
+
+    # "git://github.com/msanders/snipmate.vim.git",               # TextMate Snippets for Vim
+    # "git://github.com/tpope/vim-surround.git",                  # quoting/parenthesizing made simple
     # "git://github.com/tpope/vim-repeat.git",                    # enable repeating supported plugin maps with "."
     # "git://github.com/kchmck/vim-coffee-script.git"             # Coffeescript support for vim
 ]
