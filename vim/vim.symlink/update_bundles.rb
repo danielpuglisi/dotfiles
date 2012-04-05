e7a444c5b77b0f426ee8ba41e8ca4062e9367ad9
Rodrigo Haenggi <rodrigo@codegestalt.com>
2012-04-05 20:17:27 +0200

    update vimrc

--- vim/vim.symlink/update_bundles.rb
+++ vim/vim.symlink/update_bundles.rb
@@ -13,7 +13,7 @@ git_bundles = [
     "git://github.com/vim-scripts/taglist.vim.git",               # Source code browser
     "git://github.com/mileszs/ack.vim.git",                       # Vim plugin for the Perl module / CLI script "ack"
     "git://github.com/tpope/vim-markdown.git",                    # Vim Markdown runtime files
-    "git://github.com/lepture/vim-velocity.git",                  # Velocity extensions
+    "git://github.com/lepture/vim-velocity.git"                   # Velocity extensions
 
     # "git://github.com/msanders/snipmate.vim.git",               # TextMate Snippets for Vim
     # "git://github.com/tpope/vim-surround.git",                  # quoting/parenthesizing made simple
