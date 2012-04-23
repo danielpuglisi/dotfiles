ec904e2b28539759c1bd9b524ef20e928ca1a20d
Rodrigo Haenggi <rodrigo@codegestalt.com>
2012-04-23 21:22:09 +0200

    fix path stuff

--- system/path.zsh
+++ system/path.zsh
@@ -1,10 +1,2 @@
-export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-
-# MYSQL
-export PATH="/usr/local/mysql/bin:$PATH"
-
-# TOMCAT
-export PATH="/Users/rodrigo/tomcat6/bin:$PATH"
-
-
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:/usr/local/mysql/bin:$HOME/tomcat6/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
